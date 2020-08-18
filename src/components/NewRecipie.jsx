02d02920922dc72f9aa34357b079ed9f8688730b
R <roksana_sobczak@wp.pl>
2020-08-18 19:25:38 +0200

    add new recipies title

--- src/components/NewRecipie.jsx
+++ src/components/NewRecipie.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {NavLink} from "react-router-dom";
 // import {NavLink} from "react-router-dom";
 // import ackeeFood from '../assets/ackeeFood.png';
 
@@ -28,8 +29,13 @@ class Recipies extends Component {
 
         else{
             return(
-                <div>
-                   new recipie
+                <div className="new-recipie-container">
+                    <div className="new-recipe-title">
+                        <NavLink exact to="/"><i class="fas fa-arrow-left"></i></NavLink>
+                        <p>Add recipies</p>
+                        <NavLink exact to="/new"><i class="fas fa-plus"></i></NavLink>
+                    </div>
+                   
                 </div>
             )
 
