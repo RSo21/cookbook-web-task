0a71577ce97b9ff5dd13f244a46b986a116c6f46
R <roksana_sobczak@wp.pl>
2020-08-31 19:01:37 +0200

    add form skeleton

--- src/components/NewRecipie.jsx
+++ src/components/NewRecipie.jsx
@@ -34,7 +34,18 @@ class Recipies extends Component {
                         <NavLink exact to="/"><i class="fas fa-arrow-left"></i></NavLink>
                         <p>Add recipies</p>
                         <NavLink exact to="/new"><i class="fas fa-plus"></i></NavLink>
+                        
                     </div>
+                    <form>
+                        <label>Name of recipie<input type="text"></input></label>
+                        <label>Description<input type="text"></input></label>
+                        <p>Ingredients</p>
+                        <label>Your ingredient<input type="text"></input></label>
+                        <button><i class="fas fa-plus"></i> &nbsp; Add</button>
+                        <label>Time<input type="number"></input></label>
+
+                            
+                    </form>
                    
                 </div>
             )
